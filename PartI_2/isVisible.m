c468791ec5561cf8cc92535db12318fbf95ae185
John VanZwieten <john.vanzwieten@outlook.com>
2023-12-09 11:28:02 -0700

    Simplify isVisible.

--- PartI_2/isVisible.m
+++ PartI_2/isVisible.m
@@ -1,15 +1,8 @@
 function [vis] = isVisible(u,l,r,kc)
     global umin umax
 
-    vis = false;
-
-    if(u(1) >= 0 && u(1) <= umax(1))
-        if(u(2) >= 0 && u(2) <= umax(2))
-            if((l-r)'*kc > 0)
-                if(l'*kc<0)
-                    vis = true;
-                end
-            end
-        end
-    end
+    vis = u(1) >= 0 && u(1) <= umax(1) ...
+        && u(2) >= 0 && u(2) <= umax(2) ...
+        && (l-r)'*kc > 0 ...
+        && l'*kc<0;
 end
