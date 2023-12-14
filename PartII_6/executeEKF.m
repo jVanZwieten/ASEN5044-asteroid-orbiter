c68633e8c3ee2efe32af371e0bfd6a46de54fb9c
John VanZwieten <john.vanzwieten@outlook.com>
2023-12-14 13:56:12 -0700

    Feed EKF good P_0, Q, and R, makes it work way better.

--- PartII_6/executeEKF.m
+++ PartII_6/executeEKF.m
@@ -4,17 +4,19 @@ close all
 format longg
 setGlobalVariables()
 
-global X_0 delT_integration delT_observation t_end w_A n p
-P_0 = [10/1000*eye(3) zeros(3)
-      zeros(3) 0.5/1000/1000*eye(3)];
+global X_0 P_0 delT_integration delT_observation t_end w_A p sigma_w sigma_u
+
 
 data = load("orbitdetermination-finalproj_data_2023_11_14.mat");
-landmarkPositions = propagateLandmarksInInertialFrame(data.pos_lmks_A, delT_observation, t_end, w_A);
 X_truthNoProcessNoise = numerical.propagate(X_0, delT_integration, t_end);
-Q_noProcessNoise = [zeros(n)];
+Q = DTsys.noiseMat(sigma_w^2, delT_observation);
 
 integrationObservationRatio = delT_observation/delT_integration;
-Y_simulatedNoSignalNoise = measurement.SimulateYData(X_truthNoProcessNoise(:, (1 + integrationObservationRatio):integrationObservationRatio:end), landmarkPositions, data.R_CtoN, zeros(2), delT_observation);
-R_noSignalNoise = zeros(p);
+landmarkPositions = propagateLandmarksInInertialFrame(data.pos_lmks_A, delT_observation, t_end, w_A);
+X_truthAtObservationEpochs = X_truthNoProcessNoise(:, (1 + integrationObservationRatio):integrationObservationRatio:end);
+Y_simulatedNoSignalNoise = measurement.SimulateYData(X_truthAtObservationEpochs, landmarkPositions, data.R_CtoN, zeros(2), delT_observation);
+R = sigma_u^2*eye(p);
 
-[X_estimateNoNoise, P_noNoise] = ExtendedKalmanFilter.Filter(X_0, P_0, Q_noProcessNoise, Y_simulatedNoSignalNoise, R_noSignalNoise, delT_observation, landmarkPositions, data.R_CtoN);
+[X_estimateNoNoise, P_noNoise] = ExtendedKalmanFilter.Filter(X_0, P_0, Q, Y_simulatedNoSignalNoise, R, delT_observation, landmarkPositions, data.R_CtoN);
+
+e = X_truthAtObservationEpochs(:, 2:end) - X_estimateNoNoise;
