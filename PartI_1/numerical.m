f9c8701fc0a4fa5c6a744ad15401c479118a8426
Jared Chown <jaredchown00@gmail.com>
2023-12-15 14:09:15 -0500

    Integrated process noise to acceleration

--- PartI_1/numerical.m
+++ PartI_1/numerical.m
@@ -11,18 +11,18 @@ classdef numerical
             end
         end
 
-        function state_step = rk4_state(state,step)
+        function state_step = rk4_state(state,step,gammaW)
 
-            k1 = f.twoBody(state)+f.solarRadPress();
+            k1 = f.twoBody(state,gammaW)+f.solarRadPress();
             
             statea = state+(.5*k1*step);
-            k2 = f.twoBody(statea)+f.solarRadPress();
+            k2 = f.twoBody(statea,gammaW)+f.solarRadPress();
             
             stateb = state+(.5*k2*step);
-            k3 = f.twoBody(stateb)+f.solarRadPress();
+            k3 = f.twoBody(stateb,gammaW)+f.solarRadPress();
             
             statec = state+(k3*step);
-            k4 = f.twoBody(statec)+f.solarRadPress();
+            k4 = f.twoBody(statec,gammaW)+f.solarRadPress();
 
             state_step = state+(k1+2*k2+2*k3+k4)*step/6;
             
